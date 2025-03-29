28cdedd4fce3f4e52597e35e58e068dbac67abfa
vananhtruong <91619925+vananhtruong@users.noreply.github.com>
2025-03-29 16:11:09 +0700

    Update LunaContext.cs

--- LunaBusinessObject/LunaContext.cs
+++ LunaBusinessObject/LunaContext.cs
@@ -76,15 +76,9 @@ public partial class LunaContext : DbContext
 
     public virtual DbSet<UseService> UseServices { get; set; }
 
-    private string GetConnectionString()
-    {
-        IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true, true).Build();
-        return configuration["ConnectionStrings:DefaultConnectionString"];
-    }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer(GetConnectionString());
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local);Database=Luna;User ID=sa;Password=123;Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
